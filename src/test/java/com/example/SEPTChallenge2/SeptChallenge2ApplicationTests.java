9f0197735220be8ce66ae70635f946a2c35025a2
packofgum601 <josephpackham@outlook.com>
2022-10-10 13:53:47 +1100

    first commit

---
+++ src/test/java/com/example/SEPTChallenge2/SeptChallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SEPTChallenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeptChallenge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
